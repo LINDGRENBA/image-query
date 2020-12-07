05fcfaf716402070c462b9a21b7324e9625a0a37
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-07 15:23:44 -0800

    Update project directory title.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 const App = () => {
   return (
   <div>
-    <SearchBar />
+    <SearchBar className="ui container" style={{marginTop: '10px'}}/>
   </div>
   );
 };
